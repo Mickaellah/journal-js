d2a647e8509a9981d3ca1b1ac3be8fc2d0caca73
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-06-16 16:14:59 +0300

    turn back to work in the project which is about creating journal

--- journal.js
+++ journal.js
@@ -1,3 +1,4 @@
+// This is an array which contains three objects of journals or entries.
 let journals = [
   {
     title: "The best journey",
@@ -14,30 +15,52 @@ let journals = [
 ]
 console.log(journals);
 
-let myFirstJournal = prompt("Welcome to my first journal.\nChoose (1) for listing all the entries.\nChoose (2) for adding a new entry.\nChoose (3) to quit.");
+// A variable which contains all the choices that users can choose.
+let menuOfChoice = `
+Choose (1) for listing all the entries.
+Choose (2) for adding a new entry.
+Choose (3) to quit.
+Choose (4) to delete the last entry.
+Choose (5) to delete a specific entry.
+`;
 
+// A prompt for the choices.
+let userChoice = Number(prompt(menuOfChoice));
 
-for (let i = 0; i <= journals.length; i++) {
-  if (journals === 1);
-  alert(JSON.stringify(journals[i]));
-}
+// A while loop which loop through the objects and the choices untill users choose to quit the page.
+while (userChoice !== 3) {
 
-
-let userEntryTitle = prompt("Enter your journal title.");
-let userEntryContent = prompt("Enter your journal content.");
-
-if (journals === 2);
-let newEntry = {
-  title: "The best time in your life.",
-  content: "The time which you live next to your family and your old friends is the time when you feel that you have the priveledge of life than anyone else."
-}
-journals.push(newEntry);
-alert(JSON.stringify(newEntry));
-
-
-let wantToQuit = prompt("Do you want to show the list of entries again? (yes, no)");
-if (journals === 3) {
-
+// Switch statement that stores all the data and all the conditions for this journal to work.
+  switch(userChoice) {
+    case 1:
+      for (let i = 0; i < journals.length; i++) {
+        let entryString = `Title: ${journals[i].title}
+Content: ${journals[i].content}`;
+        alert(entryString);
+      }
+      break;
+    case 2:
+      let userEntryTitle = prompt("Enter your journal title.");
+      let userEntryContent = prompt("Enter your journal content.");
+      let entry = {
+        title: userEntryTitle,
+        content: userEntryContent
+      }
+      journals.push(entry);
+      break;
+    case 3:
+      break;
+    case 4:
+      let deletedEntry = journals.pop();
+      alert(`We deleted the last entry: ${deletedEntry.title}`);
+      break;
+    case 5:
+      let indexToDelete = Number(prompt(`Enter the entry's index to delete (1 to ${journals.length})`));
+      let entryDeleted = journals.splice(indexToDelete--, 1);
+      alert(`You just deleted ${entryDeleted[0].title}`);
+      break;
+  }
+  userChoice = Number(prompt(menuOfChoice));
 }
 
 
